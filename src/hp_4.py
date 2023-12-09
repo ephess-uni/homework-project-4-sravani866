1e9a0236d4adb75fb6696cab4222ab0b4510fd26
sravani tipoju <150308000+sravani866@users.noreply.github.com>
2023-12-09 09:02:32 -0600

    hp_4
    completed.

--- src/hp_4.py
+++ src/hp_4.py
@@ -1,5 +1,6 @@
 # hp_4.py
 #
+import csv
 from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
@@ -8,27 +9,55 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    return [datetime.strptime(date, '%Y-%m-%d').strftime('%d %b %Y') for date in dates]
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinstance(start, str):
+        raise TypeError("start should be of type str")
+    if not isinstance(n, int):
+        raise TypeError("n should be of type int")
+
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    return [start_date + timedelta(days=i) for i in range(n)]
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    date_objects = date_range(start_date, len(values))
+    return list(zip(date_objects, values))
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    late_fees_dict = defaultdict(float)
+
+    with open(infile, 'r') as csvfile:
+        reader = DictReader(csvfile)
+        
+        for row in reader:
+            patron_id = row['patron_id']
+            date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
+            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%Y')
+            
+            if date_returned > date_due:
+                days_late = (date_returned - date_due).days
+                late_fee = days_late * 0.25
+                late_fees_dict[patron_id] += late_fee
+
+    with open(outfile, 'w', newline='') as csvfile:
+        fieldnames = ['patron_id', 'late_fees']
+        writer = DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        for patron_id, late_fee in late_fees_dict.items():
+            writer.writerow({'patron_id': patron_id, 'late_fees': "{:.2f}".format(late_fee)})
 
 
 # The following main selection block will only run when you choose
