85ea7f1dc9ced5361c4c54aaa73d154f616c5185
sravani tipoju <150308000+sravani866@users.noreply.github.com>
2023-12-12 09:14:58 -0600

    hp_4

--- src/hp_4.py
+++ src/hp_4.py
@@ -1,82 +1,57 @@
-import csv
 from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
 
-
 def reformat_dates(old_dates):
-    """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
-    element to a format dd mmm yyyy--01 Jan 2001."""
-    return [datetime.strptime(date, '%Y-%m-%d').strftime('%d %b %Y') for date in old_dates]
-
+    """Reformats a list of date strings in format 'yyyy-mm-dd' to 'dd mmm yyyy--01 Jan 2001'."""
+    return [datetime.strptime(date, "%Y-%m-%d").strftime("%d %b %Y") for date in old_dates]
 
 def date_range(start, n):
-    """For input date string start, with format 'yyyy-mm-dd', returns
-    a list of of n datetime objects starting at start where each
-    element in the list is one day after the previous."""
-    if not isinstance(start, str):
-        raise TypeError("start should be of type str")
-    if not isinstance(n, int):
-        raise TypeError("n should be of type int")
-
-    start_date = datetime.strptime(start, '%Y-%m-%d')
-    return [start_date + timedelta(days=i) for i in range(n)]
-
+    """Generates a list of n datetime objects starting at the given start date."""
+    if not isinstance(start, str) or not isinstance(n, int):
+        raise TypeError("Invalid input types.")
+    
+    return [datetime.strptime(start, "%Y-%m-%d") + timedelta(days=i) for i in range(n)]
 
 def add_date_range(values, start_date):
-    """Adds a daily date range to the list values beginning with
-    start_date.  The date, value pairs are returned as tuples
-    in the returned list."""
-    date_objects = date_range(start_date, len(values))
-    return list(zip(date_objects, values))
+    """Adds a daily date range to the list values beginning with start_date."""
+    return [(datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days=i), value) for i, value in enumerate(values)]
 
+def calculate_late_fees(record):
+    """Calculates late fees based on the given record."""
+    late_fee_days = (datetime.strptime(record['date_returned'], '%m/%d/%Y') - 
+                     datetime.strptime(record['date_due'], '%m/%d/%Y')).days
+    return round(max(late_fee_days * 0.25, 0), 2)
 
 def fees_report(infile, outfile):
-    """Calculates late fees per patron id and writes a summary report to
-    outfile."""
-    late_fees_dict = defaultdict(float)
+    """Calculates late fees per patron id and writes a summary report to outfile."""
+    late_fees_by_patron = defaultdict(float)
 
-    with open(infile, 'r') as csvfile:
-        reader = DictReader(csvfile)
-        
-        for row in reader:
-            patron_id = row['patron_id']
-            date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
-            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%Y')
-            
-            if date_returned > date_due:
-                days_late = (date_returned - date_due).days
-                late_fee = days_late * 0.25
-                late_fees_dict[patron_id] += late_fee
+    with open(infile) as file:
+        reader = DictReader(file)
+        for record in reader:
+            patron_id = record['patron_id']
+            late_fees = calculate_late_fees(record)
+            late_fees_by_patron[patron_id] += late_fees
 
-    with open(outfile, 'w', newline='') as csvfile:
+    late_fees_data = [{'patron_id': patron_id, 'late_fees': '{:.2f}'.format(late_fees)} for patron_id, late_fees in late_fees_by_patron.items()]
+
+    with open(outfile, "w", newline="") as file:
         fieldnames = ['patron_id', 'late_fees']
-        writer = DictWriter(csvfile, fieldnames=fieldnames)
-        
+        writer = DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
-        for patron_id, late_fee in late_fees_dict.items():
-            writer.writerow({'patron_id': patron_id, 'late_fees': "{:.2f}".format(late_fee)})
+        writer.writerows(late_fees_data)
 
-            
-
-
-# The following main selection block will only run when you choose
-# "Run -> Module" in IDLE.  Use this section to run test code.  The
-# template code below tests the fees_report function.
-#
-# Use the get_data_file_path function to get the full path of any file
-# under the data directory.
+# The following main block will run when you choose "Run -> Module" in IDLE.
+# Use this section to run test code.
 
 if __name__ == '__main__':
-    
     try:
         from src.util import get_data_file_path
     except ImportError:
         from util import get_data_file_path
 
-    # BOOK_RETURNS_PATH = get_data_file_path('book_returns.csv')
     BOOK_RETURNS_PATH = get_data_file_path('book_returns_short.csv')
-
     OUTFILE = 'book_fees.csv'
 
     fees_report(BOOK_RETURNS_PATH, OUTFILE)
@@ -84,18 +59,3 @@ if __name__ == '__main__':
     # Print the data written to the outfile
     with open(OUTFILE) as f:
         print(f.read())
-
-    def test___fees_report___includes_all_patrons(fees_report_out_short):
-        expected_fees = {
-            '17-873-8783': '15.00',
-            '83-279-0036': '0.00'
-        }
-        actual_patrons = [fee['patron_id'] for fee in fees_report_out_short]
-        expected_patrons = list(expected_fees.keys())
-
-        # Ensure that all expected patrons are present in the actual patrons list
-        for patron in expected_patrons:
-            assert patron in actual_patrons
-
-        # Ensure that there are no extra patrons in the actual patrons list
-        assert sorted(actual_patrons) == sorted(expected_patrons)    
